f45fbdff0c54d59d8e833736f85281d6c65d2869
disa <dis@mail.rotf.pl>
2018-12-02 13:45:06 +0100

    aoc day 1+2

---
+++ 1b.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2018
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var sr = new StreamReader("input1.txt");
+			int sum = 0;
+			List<int> input = new List<int>();
+
+			//List<int> freqList = new List<int>(); //90 000ms execution time
+			HashSet<int> freqList = new HashSet<int>(); // 24ms execution time
+
+			var watch = System.Diagnostics.Stopwatch.StartNew();
+			while (sr.Peek() != -1)
+			{
+				input.Add(Int32.Parse(sr.ReadLine()));
+			}
+
+			while (true)
+			{
+				foreach(int number in input)
+				{
+					sum += number;
+					if (freqList.Contains(sum))
+					{
+						Console.WriteLine(sum);
+						watch.Stop();
+						Console.WriteLine("Execution time: " + watch.ElapsedMilliseconds + "ms");
+						return;
+					}
+					freqList.Add(sum);
+				}
+			}
+		}
+	}
+}
---
+++ 2a.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2018
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int count2 = 0;
+			int count3 = 0;
+			while (Console.In.Peek() != -1)
+			{
+				bool line2 = false;
+				bool line3 = false;
+				string line = new string(Console.ReadLine().OrderBy(c => c).ToArray()) + " ";//different last char - for easier checks in loop
+
+				int count = 1;
+				char ch = line[0];
+				for (int i = 1; i < line.Length; i++)
+				{
+					if (ch == line[i])
+					{
+						count++;
+					}
+					else
+					{
+						if (count == 2)
+						{
+							line2 = true;
+						}
+						else if (count == 3)
+						{
+							line3 = true;
+						}
+						ch = line[i];
+						count = 1;
+					}
+
+				}
+
+				if (line2) count2++;
+				if (line3) count3++;
+			}
+			Console.WriteLine(count2 * count3);
+		}
+	}
+}
