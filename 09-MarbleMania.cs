f16abff15865b68d7d48c44fab9bb1772fadf1b2
disa <disaderp@users.noreply.github.com>
2018-12-10 16:20:53 +0100

    day 9+10

---
+++ 09-MarbleMania.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2018
+{
+	class Program
+	{
+		static StreamReader sr = new StreamReader("input9.txt");
+		static void Main(string[] args)
+		{
+			string data = sr.ReadLine();
+			int players = Int32.Parse(data.Split(' ')[0]);
+			int last = Int32.Parse(data.Split(' ')[6]);
+
+			//PART1
+			List<int> marbles = new List<int>(); marbles.Add(0);
+			int current = 0;
+			int[] playerScore = new int[players];
+			int currentPlayer = 0;
+			int nextMarble = 1;
+			while (true)
+			{
+				if (nextMarble % 23 == 0) //lucky marble
+				{
+					int score = marbles[mod(current - 7, marbles.Count)] + nextMarble;
+					playerScore[currentPlayer] += score;
+
+					current = mod(current - 7, marbles.Count);
+					marbles.RemoveAt(current);
+					current = mod(current, marbles.Count);
+		
+					nextMarble++;
+				}
+				else
+				{
+					current = mod(current + 2, marbles.Count);
+					marbles.Insert(current, nextMarble);//O(n)
+					nextMarble++;
+				}
+				currentPlayer = mod(currentPlayer + 1 , players);
+				if (nextMarble == last) break;//last marble score
+			}
+			Console.WriteLine(playerScore.Max());
+			//PART1
+
+			//PART2
+			Circle circle = new Circle(); circle.Init();//much faster - with custom list
+			long [] playerScore2 = new long [players];
+			int currentPlayer2 = 0;
+			int nextMarble2 = 1;
+			while (true)
+			{	
+				if (nextMarble2 % 23 == 0) //lucky marble
+				{
+					circle = circle.Prev.Prev.Prev.Prev.Prev.Prev;
+					int score = circle.Prev.Id + nextMarble2;
+					playerScore2[currentPlayer2] += score;
+
+					circle.RemovePrev();
+
+					nextMarble2++;
+				}
+				else
+				{
+					circle = circle.Next;
+					circle.Insert(nextMarble2);//O(1)
+					circle = circle.Next;
+					nextMarble2++;
+				}
+				currentPlayer2 = mod(currentPlayer2 + 1, players);
+				if (nextMarble2 == (last * 100)) break;//last marble score
+			}
+			Console.WriteLine(playerScore2.Max());
+			//PART2
+		}
+		static int mod(int x, int m)
+		{
+			int r = x % m;
+			return r < 0 ? r + m : r;
+		}
+	}
+	class Circle
+	{
+		private Circle _prev;
+		private Circle _next;
+		private int _num;
+		public void Init ()
+		{
+			this.Id = 0;
+			this.Prev = this;
+			this.Next = this;
+		}
+
+		public int Id
+		{
+			get { return _num; }
+			set { _num = value; }
+		}
+		public Circle Prev
+		{
+			get { return _prev; }
+			set { _prev = value; }
+		}
+		public Circle Next
+		{
+			get { return _next; }
+			set { _next = value; }
+		}
+		public void Insert(int num)
+		{
+			Circle t = new Circle();
+			t.Id = num;
+			t.Prev = this;
+			t.Next = this.Next;
+
+			this.Next = t;
+			this.Next.Next.Prev = t;
+		}
+		public void RemovePrev()
+		{
+			Circle toRem = this.Prev;
+			toRem.Prev.Next = this;
+			this.Prev = toRem.Prev;
+			toRem = null;
+		}
+	}
+}
---
+++ 10-TheStarsAlign.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2018
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int[]> points = new List<int[]>();
+			List<int[]> pointsOrig = new List<int[]>();
+			StreamReader sr = new StreamReader("input10.txt");
+			while (sr.Peek() != -1)
+			{
+				int[] t = new int[4];
+				int[] t2 = new int[4];
+				string line = sr.ReadLine();
+
+				int start_index = line.IndexOf('<') + 1;
+				int end_index = line.IndexOf('>');
+				int length = end_index - start_index;
+				string xy = line.Substring(start_index, length);
+
+				t[0] = Int32.Parse(xy.Split(',')[0]);
+				t[1] = Int32.Parse(xy.Split(',')[1]);
+
+				start_index = line.LastIndexOf('<') + 1;
+				end_index = line.LastIndexOf('>');
+				length = end_index - start_index;
+				xy = line.Substring(start_index, length);
+
+				t[2] = Int32.Parse(xy.Split(',')[0]);
+				t[3] = Int32.Parse(xy.Split(',')[1]);
+				points.Add(t);
+				t.CopyTo(t2, 0);//copy values not references
+				pointsOrig.Add(t2);
+			}
+
+			//PART1
+			int smallestI = 0;
+			int smallestBox = Int32.MaxValue;
+			for (int i = 0; i < 15000; i++)
+			{
+				int minx = Int32.MaxValue;
+				int miny = Int32.MaxValue;
+				int maxx = 0;
+				int maxy = 0;
+				
+				foreach (int[] t in points)
+				{
+					t[0] += t[2];
+					t[1] += t[3];
+					if (t[0] > maxx) maxx = t[0];
+					if (t[0] < minx) minx = t[0];
+
+					if (t[1] > maxy) maxy = t[1];
+					if (t[1] < miny) miny = t[1];
+				}
+				if (maxx - minx + maxy - miny < smallestBox)
+				{
+					smallestBox = maxx - minx + maxy - miny;
+					smallestI = i;
+				}
+			}
+
+			int minx2 = Int32.MaxValue;
+			int miny2 = Int32.MaxValue;
+			int maxx2 = 0;
+			int maxy2 = 0;
+			foreach (int[] t in pointsOrig)
+			{
+				t[1] += t[3] * (smallestI+1);
+				t[0] += t[2] * (smallestI+1);
+				if (t[0] > maxx2) maxx2 = t[0];
+				if (t[0] < minx2) minx2 = t[0];
+							   		  
+				if (t[1] > maxy2) maxy2 = t[1];
+				if (t[1] < miny2) miny2 = t[1];
+			}
+			Bitmap stars = new Bitmap(maxx2 - minx2 + 2, maxy2 - miny2 + 2);
+			Graphics starGraphics = Graphics.FromImage(stars);
+			Pen blackPen = new Pen(Brushes.Black);
+			foreach (int[] p in pointsOrig)
+			{
+				starGraphics.DrawRectangle(blackPen, normalise(p[0], minx2, maxx2, maxx2 - minx2), normalise(p[1], miny2, maxy2, maxy2 - miny2), 1, 1);
+			}
+			stars.Save("stars.bmp");
+			//PART1
+
+			//PART2
+			Console.WriteLine(smallestI+1);
+			//PART2
+		}
+		static int normalise(int val, int min, int max, int rangemax)//rangemin = 0
+		{
+			return (int)(((float)(val - min) / (float)(max - min)) *(rangemax - 0) + 0);
+		}
+	}
+}
+		
+
+	
+
